b2d34c67669fb33ae4b94ecee1bd58209e797cc2
Brendan Seabrook <brendanseabrook@brendans-macbook-pro.local>
2019-02-22 17:17:38 -0500

    Initial Commit

---
+++ CommonRaytracingFramework/CommonRaytracingFramework.h
@@ -0,0 +1,19 @@
+//
+//  CommonRaytracingFramework.h
+//  CommonRaytracingFramework
+//
+//  Created by Brendan Seabrook on 2019-02-22.
+//  Copyright © 2019 Brendan Seabrook. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CommonRaytracingFramework.
+FOUNDATION_EXPORT double CommonRaytracingFrameworkVersionNumber;
+
+//! Project version string for CommonRaytracingFramework.
+FOUNDATION_EXPORT const unsigned char CommonRaytracingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonRaytracingFramework/PublicHeader.h>
+
+
